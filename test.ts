d745612386f4f0e3077e268152efa0924bc11464
Ryan Lee <ryan@parity.io>
2024-08-15 20:09:41 -0400

    gogogoogogo

--- test.ts
+++ test.ts
@@ -1,67 +1,89 @@
-import { westend2_people } from "@polkadot-api/descriptors";
+import { polkadot_people, westend2_people } from "@polkadot-api/descriptors";
 import { Binary, createClient } from "polkadot-api";
 import { getWsProvider } from "polkadot-api/ws-provider/node";
-import { sr25519CreateDerive } from "@polkadot-labs/hdkd";
-import {
-  entropyToMiniSecret,
-  mnemonicToEntropy,
-  ss58Address,
-} from "@polkadot-labs/hdkd-helpers";
-import { getPolkadotSigner } from "@polkadot-api/signer";
 import { withLogsRecorder } from "@polkadot-api/logs-provider";
 import * as fs from "node:fs";
-import {
-  filter,
-  firstValueFrom,
-  tap,
-} from "rxjs";
-import { retryOnStale } from "./utils.js";
+import type { IterableElement } from "type-fest";
 
 const f = fs.createWriteStream("test.log");
 
-(async () => {
-  const mnemonic =
-    "XXXXXX";
+const descriptorMap = {
+  westend2: westend2_people,
+  polkadot: polkadot_people,
+} as const
 
+//// 1
+(async () => {
+  const client = createClient(
+    withLogsRecorder(
+      (l) => f.write(`${l}\n`),
+      getWsProvider("wss://polkadot-people-rpc.polkadot.io")
+    )
+  );
+  const peopleApi = client.getTypedApi(descriptorMap["polkadot"]);
+
+  type Swag = IterableElement<
+    Parameters<typeof peopleApi.tx.Utility.batch>[0]["calls"]
+  > & { type: "Identity" };
+
+  const calls: Swag[] = [
+    peopleApi.tx.Identity.set_username_for({
+      who: { type: "Id", value: "" },
+      username: Binary.fromText(""),
+      signature: { type: "Sr25519", value: Binary.fromHex("") },
+    }).decodedCall,
+  ];
+
+  peopleApi.tx.Utility.batch({ calls });
+})();
+
+//// 2
+(async () => {
   const client = createClient(
     withLogsRecorder(
       (l) => f.write(`${l}\n`),
       getWsProvider("wss://westend-people-rpc.polkadot.io")
     )
   );
-  const peopleApi = client.getTypedApi(westend2_people);
+  const peopleApi = client.getTypedApi(descriptorMap["westend2"]);
 
-  const entropy = mnemonicToEntropy(mnemonic);
-  const miniSecret = entropyToMiniSecret(entropy);
-  const derive = sr25519CreateDerive(miniSecret);
+  type Swag = IterableElement<
+    Parameters<typeof peopleApi.tx.Utility.batch>[0]["calls"]
+  > & { type: "Identity" };
 
-  const alice = derive("//Alice");
-  const bob = derive("//Bob");
-
-  const ss58Prefix = peopleApi.constants.System.SS58Prefix(
-    await peopleApi.compatibilityToken
-  );
-
-  await firstValueFrom(
+  const calls: Swag[] = [
     peopleApi.tx.Identity.set_username_for({
-      who: { type: "Id", value: ss58Address(bob.publicKey, ss58Prefix) },
-      username: Binary.fromText("asdf18"),
-      signature: {
-        type: "Sr25519",
-        value: Binary.fromBytes(
-          bob.sign(new TextEncoder().encode("asdf18.ryan123"))
-        ),
-      },
-    })
-      .signSubmitAndWatch(
-        getPolkadotSigner(alice.publicKey, "Sr25519", alice.sign)
-      )
-      .pipe(
-        tap({ next: (e) => console.log(e) }),
-        retryOnStale({ maxRetries: 10, initialDelay: 1000 }),
-        filter((e) => e.type === "txBestBlocksState" && e.found === true)
-      )
-  );
+      who: { type: "Id", value: "" },
+      username: Binary.fromText(""),
+      signature: { type: "Sr25519", value: Binary.fromHex("") },
+    }).decodedCall,
+  ];
 
-  client.destroy();
+  peopleApi.tx.Utility.batch({ calls });
+})();
+
+//// 3
+(async () => {
+  const client = createClient(
+    withLogsRecorder(
+      (l) => f.write(`${l}\n`),
+      getWsProvider("wss://westend-people-rpc.polkadot.io")
+    )
+  );
+  const network: keyof typeof descriptorMap = "westend2" as keyof typeof descriptorMap;
+  const peopleApi = client.getTypedApi(descriptorMap[network]);
+
+  type Swag = IterableElement<
+    Parameters<typeof peopleApi.tx.Utility.batch>[0]["calls"]
+  > & { type: "Identity" };
+
+  const calls: Swag[] = [
+    peopleApi.tx.Identity.set_username_for({
+      who: { type: "Id", value: "" },
+      username: Binary.fromText(""),
+      signature: { type: "Sr25519", value: Binary.fromHex("") },
+    }).decodedCall,
+  ];
+
+  peopleApi.tx.Utility.batch({ calls });
 })();
